36502d6272cebd8a32fbd6064add243091c39f2e
Garin87 <kamrader@gmail.com>
2018-10-28 15:57:05 +0300

    style: remove spaces

--- test/make/test.js
+++ test/make/test.js
@@ -15,7 +15,6 @@ describe('make', () => {
     const check = make(15,34,5,6,8)(-3, 21)(-1)(sum);
     assert.deepEqual(check, 85);
   }); 
-
   it('test 3', () => {
     const check = make(15,34,5,6,sum);
     assert.deepEqual(check, 60);
@@ -29,12 +28,10 @@ describe('make', () => {
     const check = make(0,sum);
     assert.deepEqual(check,0);
   });
-
   it('test 6', () => {
     const check = make(-1,-2,-2,2,sum);
     assert.deepEqual(check, -3);
   }); 
-
   it('test 7', () => {
     const check = make(1)(2)(3)(3)(4)(1)(1)(1)(-2)(-2)(-1.5,sum);
     assert.deepEqual(check, 10.5);
