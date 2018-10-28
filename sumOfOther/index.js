881f68097d123b3f66187828e9b10dabebfcd072
Garin87 <kamrader@gmail.com>
2018-10-28 15:20:42 +0300

    feat: add mocha test for function make, sumOfOther; add package.json

--- sumOfOther/index.js
+++ sumOfOther/index.js
@@ -1,5 +1,6 @@
 
-function sumOfOther(arr){
+module.exports = function sumOfOther(arr){
+	if(!Array.isArray(arr)) throw Error('Invalid argument');
 	const sum = (accumulator, currentValue) => accumulator + currentValue; 
 	let sumOther = arr.map(function(num) {
 		return arr.reduce(sum) - num;
